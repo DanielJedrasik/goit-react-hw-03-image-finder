2f76f6c4940d3cda6c708999366c254c8d5a74ec
DanielJedrasik <djedrasik3@gmail.com>
2024-02-24 15:45:38 +0100

    Complete homework

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import { Puff } from 'react-loader-spinner';
+
+class CustomLoader extends Component {
+  render() {
+    return (
+      <div className="loader">
+        <Puff type="Puff" color="#00BFFF" height={100} width={100} />
+      </div>
+    );
+  }
+}
+
+export default CustomLoader;
